471d8aa6d1fb07b67004d00733f416cf25c1939e
Onii-Chaan <martinnc@inbox.lv>
2020-01-21 23:00:11 +0200

    21.01

--- jsFiles/loadingScreen.js
+++ jsFiles/loadingScreen.js
@@ -21,13 +21,13 @@ var loadingScreen = {
                 fadeTarget.style.opacity = 1;
             }
             if (fadeTarget.style.opacity > 0) {
-                fadeTarget.style.opacity -= 0.05;
+                fadeTarget.style.opacity -= 0.1;
             } else {
                 clearInterval(fadeEffect);
                 fadeTarget.style.display = 'none';
 
             }
-        }, 50);
+        }, 10);
     }
 };
 
